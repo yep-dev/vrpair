6acbf8e0a225d655a3312dfd9ae5635ccfccf403
yep-dev <yep.dev7@gmail.com>
2021-11-25 15:02:31 +0100

    Add first ts enum, for gender

---
+++ app/app/utils/enums.ts
@@ -0,0 +1,30 @@
+const genderEnum = {
+  male: {
+    value: 1,
+    label: "Male",
+  },
+  maleTrans: {
+    value: 2,
+    label: "Transsexual Male",
+  },
+  female: {
+    value: 5,
+    label: "Female",
+  },
+  femaleTrans: {
+    value: 6,
+    label: "Transsexual Female",
+  },
+  other: {
+    value: 9,
+    label: "Other",
+  },
+}
+
+export enum GenderValues {
+  male = 1,
+  maleTrans = 2,
+  female = 5,
+  femaleTrans = 6,
+  other = 9,
+}
