be13d7af34e2e1878f7b2258e9e5b8beac617b19
yep-dev <yep.dev7@gmail.com>
2021-12-04 23:42:27 +0100

    Inlcude enums labels/colors mapping, remove its number values

--- app/app/utils/enums.ts
+++ app/app/utils/enums.ts
@@ -1,30 +1,79 @@
-const genderEnum = {
-  male: {
-    value: 1,
-    label: "Male",
+export const enums = {
+  gender: {
+    male: {
+      label: "Male",
+      color: "blue",
+    },
+    maleTrans: {
+      label: "Trans Male",
+      color: "blue",
+    },
+    female: {
+      label: "Female",
+      color: "pink",
+    },
+    femaleTrans: {
+      label: "Trans Female",
+      color: "pink",
+    },
+    nonBinary: {
+      label: "Non-binary",
+      color: "gray",
+    },
   },
-  maleTrans: {
-    value: 2,
-    label: "Transsexual Male",
+  setup: {
+    quest: {
+      label: "Quest Standalone",
+    },
+    pcvr: {
+      label: "PC Half-Body",
+    },
+    fbt: {
+      label: "FBT",
+    },
   },
-  female: {
-    value: 5,
-    label: "Female",
+  role: {
+    sub: {
+      label: "Sub",
+      color: "pink",
+    },
+    dom: {
+      label: "Dom",
+      color: "blue",
+    },
+    switch: {
+      label: "Switch",
+      color: "green",
+    },
   },
-  femaleTrans: {
-    value: 6,
-    label: "Transsexual Female",
+  mute: {
+    true: {
+      label: "Mute Only",
+    },
+    false: {
+      label: "Not Mute",
+    },
   },
-  other: {
-    value: 9,
-    label: "Other",
+  furry: {
+    true: {
+      label: "Furry Only",
+    },
+    false: {
+      label: "Not Furry",
+    },
   },
-}
-
-export enum GenderValues {
-  male = 1,
-  maleTrans = 2,
-  female = 5,
-  femaleTrans = 6,
-  other = 9,
+  femAvatar: {
+    true: {
+      label: "Feminine Avatar",
+      color: "pink",
+    },
+    false: {
+      label: "Masculine Avatar",
+      color: "blue",
+    },
+    any: {
+      label: "Any Gender Avatar",
+      color: "green",
+    },
+  },
 }
