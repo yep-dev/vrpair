5bf4e5e25085384c9ad20624df95f40781782f57
yep-dev <yep.dev7@gmail.com>
2021-12-07 16:42:25 +0100

    Restrict aliased native-base components

--- app/app/components/Profile/Preferences.tsx
+++ app/app/components/Profile/Preferences.tsx
@@ -1,6 +1,6 @@
 import { TPreferences } from "api/profiles"
-import { Tag } from "components/Profile/Profile"
-import { Row, Text, VStack } from "native-base"
+import { Tag, TagRow } from "components/Profile/Profile"
+import { Text, Column } from "native-base"
 import React, { FC } from "react"
 import { enums } from "utils/enums"
 
@@ -8,12 +8,12 @@ const Preferences: FC<{ preferences: TPreferences }> = ({ preferences }) => {
   const genderIncludes = (elements) => elements.every((value) => preferences.gender.includes(value))
 
   return (
-    <VStack>
+    <Column>
       <Text mb={2}>Preferences</Text>
       {genderIncludes(Object.keys(enums.gender)) ? (
         <Tag colorScheme="green">Everyone</Tag>
       ) : (
-        <Row space={2}>
+        <TagRow space={2}>
           {["maleCis", "maleTrans"].some((value) => preferences.gender.includes(value)) && (
             <Tag colorScheme={enums.gender.male.color}>
               {genderIncludes(["maleCis", "maleTrans"])
@@ -35,19 +35,19 @@ const Preferences: FC<{ preferences: TPreferences }> = ({ preferences }) => {
           {genderIncludes(["nonBinary"]) && (
             <Tag colorScheme={enums.gender.nonBinary.color}>Non-Binary</Tag>
           )}
-        </Row>
+        </TagRow>
       )}
       <Tag colorScheme={enums.femAvatar[preferences.femAvatar].label}>
         {enums.femAvatar[preferences.femAvatar].label}
       </Tag>
-      <Row space={2}>
+      <TagRow space={2}>
         {preferences.role.map((role) => (
           <Tag key={role} colorScheme={enums.role[role].color}>
             {enums.role[role].label}
           </Tag>
         ))}
-      </Row>
-      <Row space={2}>
+      </TagRow>
+      <TagRow space={2}>
         {Object.keys(enums.setup).every((value) => preferences.setup.includes(value)) ? (
           <Tag colorScheme="green">Any VR setup</Tag>
         ) : (
@@ -57,16 +57,16 @@ const Preferences: FC<{ preferences: TPreferences }> = ({ preferences }) => {
             </Tag>
           ))
         )}
-      </Row>
-      <Row space={2}>
+      </TagRow>
+      <TagRow space={2}>
         {preferences.mute !== "any" && (
           <Tag colorScheme="gray">{enums.mute[preferences.mute.toString()].label}</Tag>
         )}
         {preferences.furry !== "any" && (
           <Tag colorScheme="gray">{enums.furry[preferences.furry.toString()].label}</Tag>
         )}
-      </Row>
-    </VStack>
+      </TagRow>
+    </Column>
   )
 }
 
--- app/app/components/Profile/Profile.tsx
+++ app/app/components/Profile/Profile.tsx
@@ -1,11 +1,11 @@
 import { TProfile } from "api/profiles"
 import Preferences from "components/Profile/Preferences"
-import { Badge, Flex, HStack, Image, Text, VStack } from "native-base"
+import { Badge, Flex, Image, Text, Row, Column } from "native-base"
 import React, { FC } from "react"
 import styled from "styled-components"
 import { enums } from "utils/enums"
 
-const Row = styled(HStack)`
+export const TagRow = styled(Row)`
   flex-wrap: wrap;
 `
 
@@ -26,25 +26,25 @@ export const Profile: FC<{ profile: TProfile }> = ({ profile }) => {
         style={{ height: 200 }} // todo: constant aspect ratio
         mb={8}
       />
-      <VStack mb={4}>
+      <Column mb={4}>
         <Text fontSize="2xl" mb={2}>
           {profile.username}, {profile.age}
         </Text>
-        <Row space={2}>
+        <TagRow space={2}>
           <Tag colorScheme={enums.gender[profile.gender.replace("Cis", "")].color}>
             {enums.gender[profile.gender.replace("Cis", "")].label}
           </Tag>
           <Tag colorScheme={enums.femAvatar[profile.femAvatar.toString()].color}>
             {enums.femAvatar[profile.femAvatar.toString()].label}
           </Tag>
-        </Row>
-        <Row space={2}>
+        </TagRow>
+        <TagRow space={2}>
           <Tag colorScheme={enums.role[profile.role].color}>{enums.role[profile.role].label}</Tag>
           <Tag colorScheme="gray">{enums.setup[profile.setup].label}</Tag>
           {profile.mute && <Tag colorScheme="gray">Mute</Tag>}
           {profile.furry && <Tag colorScheme="gray">Furry</Tag>}
-        </Row>
-      </VStack>
+        </TagRow>
+      </Column>
       {preferences && <Preferences preferences={profile.preferences} />}
     </Flex>
   )
