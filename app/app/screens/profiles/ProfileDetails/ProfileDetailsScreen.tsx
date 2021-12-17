bc9f40ca947628615cab8c3c90a697c77952fd90
yep-dev <yep.dev7@gmail.com>
2021-12-17 19:02:01 +0100

    Use liking api in the profile details screen

--- app/app/screens/profiles/ProfileDetails/ProfileDetailsScreen.tsx
+++ app/app/screens/profiles/ProfileDetails/ProfileDetailsScreen.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from "react"
 
 import { RouteProp } from "@react-navigation/native"
+import { useMutation } from "react-query"
 
+import { useApi } from "api/apiProvider"
 import { Screen } from "components"
 import { Profile } from "components/Profile/Profile"
 import { ProfileOverlays } from "components/ProfileOverlays/ProfileOverlays"
@@ -12,12 +14,28 @@ type Props = {
 }
 
 export const ProfileDetailsScreen: FC<Props> = ({ route }) => {
-  const { profile } = route.params
+  const api = useApi()
+  const { profile, liked, skipped } = route.params
+  const likeProfile = useMutation(api.likes.likeProfile)
+  const skipProfile = useMutation(api.likes.skipProfile)
+
+  const handleLike = () => {
+    likeProfile.mutate({ profileId: profile.id })
+  }
+
+  const handleSkip = () => {
+    skipProfile.mutate({ profileId: profile.id })
+  }
 
   return (
     <Screen>
       <Profile profile={profile} />
-      <ProfileOverlays />
+      <ProfileOverlays
+        handleLike={handleLike}
+        handleSkip={handleSkip}
+        liked={liked}
+        skipped={skipped}
+      />
     </Screen>
   )
 }
