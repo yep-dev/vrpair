9ed702c72010ab39574e400dbcc4d66e50afa7a1
yep-dev <yep.dev7@gmail.com>
2021-12-21 19:29:38 +0100

    Add likes/skips and pairs fixtures

--- backend/vrpair/contrib/management/commands/load_fixtures.py
+++ backend/vrpair/contrib/management/commands/load_fixtures.py
@@ -1,6 +1,14 @@
+import random
 from django.core.management.base import BaseCommand
 
+from vrpair.likes.factories import (
+    LikedProfileFactory,
+    SkippedProfileFactory,
+    PairFactory,
+)
+from vrpair.likes.models import LikedProfile
 from vrpair.profiles.factories import ProfileFactory
+from vrpair.profiles.models import Profile
 from vrpair.users.factories import UserFactory
 
 
@@ -9,5 +17,20 @@ class Command(BaseCommand):
         UserFactory.create_batch(100)
         ProfileFactory.create_batch(200, preferences=None)
         ProfileFactory.create_batch(1000)
+
+        for profile in Profile.objects.order_by("?")[:500]:
+            for author in Profile.objects.exclude(id=profile.id).order_by("?")[
+                : random.randint(0, 10) ** 2
+            ]:
+                LikedProfileFactory.create(author=author, profile=profile)
+
+        for profile in Profile.objects.order_by("?")[:500]:
+            for author in Profile.objects.exclude(id=profile.id).order_by("?")[
+                : random.randint(0, 20) ** 2
+            ]:
+                SkippedProfileFactory.create(author=author, profile=profile)
+
+        for liked_profile in LikedProfile.objects.order_by("?")[:1000]:
+            PairFactory(profile1=liked_profile.author, profile2=liked_profile.profile)
 
         self.stdout.write(self.style.SUCCESS("Full fixtures loaded"))
---
+++ backend/vrpair/likes/factories.py
@@ -0,0 +1,34 @@
+import factory
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
+from factory.django import DjangoModelFactory
+from factory.fuzzy import FuzzyDateTime
+
+from vrpair.likes.models import LikedProfile, SkippedProfile, Pair
+from vrpair.profiles.models import Profile
+
+
+class LikedProfileFactory(DjangoModelFactory):
+    author = factory.Iterator(Profile.objects.order_by("?"))
+    # profile has to be passed
+    date = FuzzyDateTime(timezone.now() - relativedelta(months=3), timezone.now())
+
+    class Meta:
+        model = LikedProfile
+
+
+class SkippedProfileFactory(DjangoModelFactory):
+    author = factory.Iterator(Profile.objects.order_by("?"))
+    # profile has to be passed
+    date = FuzzyDateTime(timezone.now() - relativedelta(months=3), timezone.now())
+
+    class Meta:
+        model = SkippedProfile
+
+
+class PairFactory(DjangoModelFactory):
+    # profile1 and profile2 have to be passed
+    date = FuzzyDateTime(timezone.now() - relativedelta(months=3), timezone.now())
+
+    class Meta:
+        model = Pair
