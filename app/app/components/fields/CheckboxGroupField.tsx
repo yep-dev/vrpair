41b6e22df3766c1e8b9810e6d7723f7f642fa6dc
yep-dev <yep.dev7@gmail.com>
2022-02-13 20:20:44 +0100

    Add missing checkbox group field

---
+++ app/app/components/fields/CheckboxGroupField.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from "react"
+
+import { Checkbox, Column, ICheckboxProps } from "native-base"
+import { useController } from "react-hook-form"
+
+import { FieldError, FieldLabel } from "components"
+import { ChoiceEnum } from "utils/enums"
+
+type Props = {
+  name: string
+  label: string
+  rules?: any
+  items: ChoiceEnum[]
+} & Omit<ICheckboxProps, "value" | "onChange">
+
+export const CheckboxGroupField: FC<Props> = ({ name, label, rules, items }) => {
+  const { field, fieldState } = useController({
+    name,
+    rules,
+    defaultValue: [],
+  })
+
+  return (
+    <Checkbox.Group onChange={field.onChange} value={field.value}>
+      {label && <FieldLabel label={label} />}
+      <Column space={3}>
+        {items.map(({ key, label }) => (
+          <Checkbox key={key} value={key}>
+            {label}
+          </Checkbox>
+        ))}
+      </Column>
+      <FieldError error={fieldState.error} />
+    </Checkbox.Group>
+  )
+}
