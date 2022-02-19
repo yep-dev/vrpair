9457b0ce0c23a89ee13e88b0235605533184bdef
yep-dev <yep.dev7@gmail.com>
2022-02-19 18:58:19 +0100

    Update app env config

--- app/app/config/env.ts
+++ app/app/config/env.ts
@@ -1,2 +1,2 @@
-export const OAUTH_DISCORD_CLIENT_ID = "893178973016702976"
-export const API_URL = "http://10.0.0.4:8000"
+export const OAUTH_DISCORD_CLIENT_ID = "922949871420461156"
+export const API_URL = "http://localhost:8000"
